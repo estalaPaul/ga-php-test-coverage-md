d0a792f6050051fecc335dbc1018d754c21c0983
Paul Estala <32235423+estalaPaul@users.noreply.github.com>
2020-09-23 06:50:55 -0600

    fix:

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,14 @@ composer install
 
 vendor/bin/phpunit --coverage-text=coverage.txt
 
-MARKDOWN=$(jq -aRs . <<< cat coverage.txt)
+markdown=$(jq -aRs . <<< cat coverage.txt)
 NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
+echo $markdown;
+
 curl -sSL \
+  -X POST \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
-  -X POST \
-  -d '{"body":"'"${MARKDOWN}"'"}' \
-  "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${NUMBER}/comments"
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${NUMBER}/comments" \
+  -d "{\"body\":\"$markdown\"}"
